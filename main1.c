9c70cc4b0ce3d264c6b479ee3209556c182541d9
Truong <daongoctruong10042000@gmail.com>
2024-03-06 16:57:26 +0700

    modifiled_3_6

--- main1.c
+++ main1.c
@@ -18,19 +18,23 @@ struct Node{
 struct Node* head = NULL;
 
 struct Node* Add_player(){
+    // create a pointer struct and allocate for it
     struct Node* New_player = (struct Node*)malloc(sizeof(struct Node));
     if(New_player == NULL){
         printf("Fail: Memory allocation fail");
     }
-    // enter player
+
+    // enter player and assign value for each element of struct
     printf("Enter Your Player: ");
     scanf("%s",New_player->Player);
     New_player->Score = 0;
     New_player->next = NULL;
+
     // check head 
     if(head == NULL){
         head = New_player;
     }else{
+        // access to final player of linked list struct to save
         struct Node* Now = head;
         while(Now->next != NULL){
             Now = Now->next;
@@ -98,15 +102,20 @@ void compare_magicNumber(struct Node* current, int a[]){
     	dem++;
     }while(a[0] != check[0] || a[1] != check[1] || a[2] != check[2] || a[3] != check[3]);
 
+    // save score of players
     current->Score = (1/dem)*100;
 }
 
 void savetofile(struct Node* current){
-    FILE *file = fopen("Player.txt","a");
+    // open file to save data
+    FILE *file = fopen("Player.txt","a+");
+
+    //check file
     if(file == NULL){
         printf("FAIL: Open fail");
         exit(1);
     }
+    // Save the data you just played
     while(current != NULL){
         fprintf(file, "%s %.2f \n",current->Player,current->Score);
         current = current->next;
@@ -114,26 +123,37 @@ void savetofile(struct Node* current){
     fclose(file);
 }
 void printtofile(const char *filename){
+    // Open file save data
     FILE *file = fopen(filename,"r");
+    // check open
     if(file == NULL){
         printf("FAIL: Open fail");
         exit(1);
     }
+    // create name and score variable
     char name[MAX];
     float score;
+
+    // create number_player to count player in the linked list struct
     int number_player = 0 ;
     while (fscanf(file, "%s %f",name,&score) == 2) {
         number_player++;
     }
     // Rewind file pointer to the beginning
     rewind(file);
+
+    // create a struct array corresponding to the number of saved players
     struct Node players[number_player];
+
+    // assign value for each struct element
     int i = 0;
     while (fscanf(file, "%s %f",name,&score) == 2) {
         strcpy(players[i].Player, name);
         players[i].Score = score;
         i++;
     }
+
+    // Arranged from largest to smallest
     for(int i = 0; i < number_player;i++){
 		for(int j = 0; j < number_player - i;j++){
 			if(players[j].Score < players[j+1].Score){
@@ -143,9 +163,20 @@ void printtofile(const char *filename){
 			}
 		}
 	}
-	for(int i = 0;i<5;i++){
+    // print data of player
+	if(number_player < 6){
+		for(int i = 0;i<number_player;i++){
 		printf("player: %s, score: %f\n",players[i].Player,players[i].Score);
+		}
+		printf("There are only %d players\n",number_player);
 	}
+	else{
+		for(int i = 0;i<5;i++){
+		printf("player: %s, score: %f\n",players[i].Player,players[i].Score);
+		}
+	}
+
+	// check the pointer position at the end of the array
 	if (feof(file)) {
         printf("End of file reached.\n");
     } else if (ferror(file)) {
@@ -154,38 +185,98 @@ void printtofile(const char *filename){
     fclose(file);
 }
 
-int main(){
-
+// function show select
+void manual(){
+	
     printf("----------------------Number Guesting Game------------------------\n");
+	printf("Enter\'1\'. Start Game \n");
+	printf("Enter\'2\'. Exit Game \n");
+	printf("Enter\'3\'. Show High Player \n");
+	printf("Enter\'4\'. Delete All Player\n");
+    printf("Your selection: ");
+
+}
+int main(){
+	
+	
+    int choose = 0;
+    resel:
+//	nhaplai:
+    manual();              
+    scanf("%d",&choose);
+    switch (choose)
+    {
+    case 1:
+//        printf("Enter\'1\'. Start Game \n");
+        // variable to continue the loop
+        char con_ti_nue;
+
+        do{
+            // create the number player must guest
+            srand((unsigned int) time(NULL));
+            int magic_number = random (0,9999);
+            printf("magic number: %d\n",magic_number);
+
+            // save in array to easy access
+            int a[4] = {0,0,0,0};
+            Devide_number(a,magic_number);
+
+            // create New_player
+            struct Node* current = Add_player();
+
+            // compare
+            compare_magicNumber(current,a);
+            
+            printf("%s\n",current->Player);
+            printf("%.3f\n",current->Score);
+
+            // ask to continue
+            printf("Do you want to play continue? [Y/N]?");
+            scanf(" %c",&con_ti_nue);
+            // save file
+            savetofile(current);
+
+        }while(con_ti_nue == 'Y' || con_ti_nue == 'y');
+        // print file
+        printtofile("Player.txt");
+        goto resel;
+    case 2:
+        exit(0);
+        break;
+    case 3:
+        // print file
+        printtofile("Player.txt");
+        goto resel;
+    case 4:
+    {
+        // open file and select "W"
+    	FILE *f = fopen("Player.txt","w");
+    	if(f == NULL){
+			printf("Erorr: Don't open file");
+		}else{
+			printf("Delete Success Full");
+		}
+		goto resel;
+	}
+        // Detele file
+        // int ret;
+        // ret = remove("Player.txt");
+        // if(ret == 0) 
+        // {
+        //     printf("Delete Successful!!!");
+        // }
+        // else 
+        // {
+        //     printf("Error: Cannot delete above file");
+        // }
+        // goto resel;
+    default:
+        printf("Enter diffrent 1 to 4\n");
+        printf("Enter again\n");
+        goto resel;
+    }
     
-    // variable to continue the loop
-    char con_ti_nue;
-
-    do{
-        // create the number player must guest
-	    srand((unsigned int) time(NULL));
-	    int magic_number = random (0,9999);
-        printf("magic number: %d\n",magic_number);
-
-        // save in array to easy access
-	    int a[4] = {0,0,0,0};
-        Devide_number(a,magic_number);
-
-        // create New_player
-        struct Node* current = Add_player();
-
-        // compare
-        compare_magicNumber(current,a);
-		
-		printf("%s\n",current->Player);
-        printf("%.3f\n",current->Score);
-
-        // ask to continue
-        printf("Do you want to play continue? [Y/N]?");
-        scanf(" %c",&con_ti_nue);
-        savetofile(current);
-
-    }while(con_ti_nue == 'Y' || con_ti_nue == 'y');
-    printtofile("Player.txt");
+    
+    // printtofile("Player.txt");
     return 0;
 }
