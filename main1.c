187721f83a6811d072c29aebe0cc0770a0afb80c
Truong <daongoctruong10042000@gmail.com>
2024-03-08 14:26:03 +0700

    modifile_final

--- main1.c
+++ main1.c
@@ -11,40 +11,53 @@
 struct Node{
     char Player[MAX];
     float Score;
-    struct Node* next;
 };
 
-// create first player
-struct Node* head = NULL;
+int compare_name(struct Node* New_player,const char *filename){
+    FILE *fil = fopen(filename,"r");
+    if(fil == NULL){
+        printf("FAIL: Open fail");
+        exit(1);
+    }
+    char name[MAX];
+    int score;
+    // Rewind file pointer to the beginning
+    rewind(fil);
+	while(fscanf(fil, "%s %f",name,&score) == 2) {
+//		printf("Name: %s\n",name);
+//		printf("Namemoinhap: %s",New_player->Player);
+        if(strcmp(New_player->Player, name) == 0){
+            printf("This name is already in use\n");
+            fclose(fil);
+            return 1;
+        }
+    }
+    printf("Compared \n");
+    fclose(fil);
+    return 0;
+}
+
 
 struct Node* Add_player(){
-    // create a pointer struct and allocate for it
+sua:
     struct Node* New_player = (struct Node*)malloc(sizeof(struct Node));
     if(New_player == NULL){
         printf("Fail: Memory allocation fail");
+        return NULL; // Tr? v? NULL n?u không th? c?p phát b? nh?
     }
-
-    // enter player and assign value for each element of struct
+    // Nh?p tên ngư?i chơi
     printf("Enter Your Player: ");
     scanf("%s",New_player->Player);
     New_player->Score = 0;
-    New_player->next = NULL;
-
-    // check head 
-    if(head == NULL){
-        head = New_player;
-    }else{
-        // access to final player of linked list struct to save
-        struct Node* Now = head;
-        while(Now->next != NULL){
-            Now = Now->next;
-        }
-        Now->next = New_player;
-
+    if(compare_name(New_player,"Player.txt") == 1){
+        free(New_player); // N?u tên đ? t?n t?i, gi?i phóng b? nh? và th? l?i
+        goto sua;
     }
     return New_player;
-
 }
+
+
+
 // create the function random number generator
 int random(int min, int max){
     return min + rand() % (max + 1 - min);
@@ -72,6 +85,7 @@ void compare_magicNumber(struct Node* current, int a[]){
 
     int guest_number = 0;
 
+
 	//Create an array to store the number entered by the user
 	int check[4] = {0,0,0,0};
 	
@@ -81,13 +95,21 @@ void compare_magicNumber(struct Node* current, int a[]){
 		printf("Number Guesting is 0 to 9999\n");
 		nhaplai:
         printf("Enter number you guest: ");
-        scanf("%d",&guest_number);
+        if(scanf("%d", &guest_number) != 1){
+	    	printf("Enter a 4-digit number\n");
+	    	while (getchar() != '\n');
+			goto nhaplai;
+		}
         int b = guest_number / 1000;
         if(b > 9){
 			printf("You enter the number bigger the random number\n");
 			printf("Enter again\n");
 			goto nhaplai;
-		}  
+		}
+		if(guest_number < 1000){
+			printf("Enter a 4-digit number\n");
+			goto nhaplai;
+		}
         Devide_number(check,guest_number);    
 
         // Check the random number is the same
@@ -102,58 +124,42 @@ void compare_magicNumber(struct Node* current, int a[]){
     	dem++;
     }while(a[0] != check[0] || a[1] != check[1] || a[2] != check[2] || a[3] != check[3]);
 
-    // save score of players
     current->Score = (1/dem)*100;
 }
 
 void savetofile(struct Node* current){
-    // open file to save data
     FILE *file = fopen("Player.txt","a+");
-
-    //check file
     if(file == NULL){
         printf("FAIL: Open fail");
         exit(1);
     }
-    // Save the data you just played
-    while(current != NULL){
+//    while(current != NULL){
         fprintf(file, "%s %.2f \n",current->Player,current->Score);
-        current = current->next;
-    }
+//        current = current->next;
+//    }
     fclose(file);
 }
 void printtofile(const char *filename){
-    // Open file save data
     FILE *file = fopen(filename,"r");
-    // check open
     if(file == NULL){
         printf("FAIL: Open fail");
         exit(1);
     }
-    // create name and score variable
     char name[MAX];
     float score;
-
-    // create number_player to count player in the linked list struct
     int number_player = 0 ;
     while (fscanf(file, "%s %f",name,&score) == 2) {
         number_player++;
     }
     // Rewind file pointer to the beginning
     rewind(file);
-
-    // create a struct array corresponding to the number of saved players
     struct Node players[number_player];
-
-    // assign value for each struct element
     int i = 0;
     while (fscanf(file, "%s %f",name,&score) == 2) {
         strcpy(players[i].Player, name);
         players[i].Score = score;
         i++;
     }
-
-    // Arranged from largest to smallest
     for(int i = 0; i < number_player;i++){
 		for(int j = 0; j < number_player - i;j++){
 			if(players[j].Score < players[j+1].Score){
@@ -163,7 +169,6 @@ void printtofile(const char *filename){
 			}
 		}
 	}
-    // print data of player
 	if(number_player < 6){
 		for(int i = 0;i<number_player;i++){
 		printf("player: %s, score: %f\n",players[i].Player,players[i].Score);
@@ -175,8 +180,7 @@ void printtofile(const char *filename){
 		printf("player: %s, score: %f\n",players[i].Player,players[i].Score);
 		}
 	}
-
-	// check the pointer position at the end of the array
+	
 	if (feof(file)) {
         printf("End of file reached.\n");
     } else if (ferror(file)) {
@@ -185,7 +189,6 @@ void printtofile(const char *filename){
     fclose(file);
 }
 
-// function show select
 void manual(){
 	
     printf("----------------------Number Guesting Game------------------------\n");
@@ -198,12 +201,17 @@ void manual(){
 }
 int main(){
 	
-	
     int choose = 0;
     resel:
 //	nhaplai:
-    manual();              
-    scanf("%d",&choose);
+    manual();
+    
+//    scanf("%d",&choose);
+    if(scanf("%d",&choose) != 1){
+    	printf("Enter 1 or 2 or 3 or 4\n");
+    	while (getchar() != '\n');
+		goto resel;
+	}
     switch (choose)
     {
     case 1:
@@ -233,23 +241,20 @@ int main(){
             // ask to continue
             printf("Do you want to play continue? [Y/N]?");
             scanf(" %c",&con_ti_nue);
-            // save file
             savetofile(current);
+             free(current);
 
         }while(con_ti_nue == 'Y' || con_ti_nue == 'y');
-        // print file
         printtofile("Player.txt");
         goto resel;
     case 2:
         exit(0);
         break;
     case 3:
-        // print file
         printtofile("Player.txt");
         goto resel;
     case 4:
-    {
-        // open file and select "W"
+    {	
     	FILE *f = fopen("Player.txt","w");
     	if(f == NULL){
 			printf("Erorr: Don't open file");
@@ -275,7 +280,6 @@ int main(){
         printf("Enter again\n");
         goto resel;
     }
-    
     
     // printtofile("Player.txt");
     return 0;
